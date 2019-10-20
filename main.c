b1ea9ea2dce5055235a6cc6ea278805cfc593bf1
SimoLieF <55975272+SimoLieF@users.noreply.github.com>
2019-10-21 04:03:12 +0900

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     while(fgets(str, sizeof(str), stdin)){
         printf("%s",str);
         if(str=='0'){
-            for(int i=0;i<=cnt;i++){
+            for(int i=cnt;i>=0cnt;i--){
                 printf("%s",list[i].num);
                 if(i<cnt)
                     printf(",");
